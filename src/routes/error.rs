013b0a811a95d4de2738ea6db5a41668cc1d9ff5
Sergei <dev.sergeialimov@gmail.com>
2023-11-25 08:07:11 +0300

    add error.rs

--- src/routes/error.rs
+++ src/routes/error.rs
@@ -1,1 +1,21 @@
-use axum::http::
+use axum::http::StatusCode;
+
+pub enum ApiError {
+    SqlxError,
+    ReqwestError,
+}
+
+impl ApiError {
+    pub fn to_error(&self, err: String) {
+        match self {
+            ApiError::SqlxError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("SQL query error: {err}"),
+            ),
+            ApiError::ReqwestError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Fetch error: {err}"),
+            )
+        }
+    }
+}
